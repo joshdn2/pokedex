776effc3b100a6f49fb119d3aded65713951698e
Josh Danneman <joshdn2@gmail.com>
2025-02-26 17:37:12 -0500

    small cleanups

---
+++ src/app/components/pokemon-details-info/pokemon-details-info.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pokemon-details-info',
+  standalone: true,
+  imports: [],
+  templateUrl: './pokemon-details-info.component.html',
+  styleUrl: './pokemon-details-info.component.css'
+})
+export class PokemonDetailsInfoComponent {
+
+}
